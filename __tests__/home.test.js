4a4e23c07caf5983918c4ddc6c0eaebc37efe059
Rogelio <20210663@uthh.edu.mx>
2024-10-29 12:05:55 -0600

    pruebas

---
+++ __tests__/home.test.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { render, fireEvent, waitFor, mockPush } from '@testing-library/react-native';
+import Home from "../app/home"
+import { useRouter } from 'expo-router';
+
+// Mocks de dependencias
+jest.mock('expo-router', () => ({
+    useRouter: jest.fn(),
+    useNavigation: jest.fn(() => ({
+      dispatch: jest.fn(),
+    })),
+    Stack: {
+      Screen: ({ children }) => <>{children}</>, // Mock de Stack.Screen
+    },
+}));
+
+// Definir mock para `useAuth`
+jest.mock('../app/auth/useAuth', () => ({
+  useAuth: () => ({
+    login: jest.fn(),
+  }),
+}));
+
+jest.mock('react-hook-form', () => ({
+  useForm: () => ({
+    control: jest.fn(),
+    handleSubmit: jest.fn(),
+    formState: { errors: {} },
+    trigger: jest.fn(),
+    register: jest.fn(),
+  }),
+}));
+
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+}));
+
+jest.mock('react-native-paper', () => {
+    const React = require('react');
+    const { View, TextInput, Text } = require('react-native'); // Importar dentro del mock
+  
+    return {
+      Checkbox: ({ status, onPress }) => (
+        <View testID="checkbox" onPress={onPress}>
+          {status}
+        </View>
+      ),
+      TextInput: ({ label, value, onChangeText }) => (
+        <View>
+          <Text>{label}</Text>
+          <TextInput
+            value={value}
+            onChangeText={onChangeText}
+            style={{ borderColor: 'gray', borderWidth: 1 }}
+          />
+        </View>
+      ),
+    };
+  });
+
+// Mock para los inputs personalizados
+jest.mock('../components/inputs', () => {
+    const React = require('react');
+    const { View, TextInput, Text } = require('react-native');
+  
+    return {
+      CustomInput: ({ placeholder, label, errors }) => (
+        <View testID="custom-input">
+          {label && <Text>{label}</Text>}
+          <TextInput placeholder={placeholder} />
+          {errors && errors[label] && (
+            <Text style={{ color: 'red' }}>{errors[label]?.message}</Text>
+          )}
+        </View>
+      ),
+      CustomInputPassword: ({ placeholder }) => (
+        <View testID="custom-input-password">
+          <TextInput placeholder={placeholder} secureTextEntry />
+        </View>
+      ),
+      LoadingButton: ({ isLoading }) => (
+        <View testID="loading-button">
+          <Text>{isLoading ? 'Cargando...' : 'Iniciar Sesión'}</Text>
+        </View>
+      ),
+    };
+});
+
+// Definir el mock para `useRouter`
+describe("Login renderizado", () => {
+    const mockPush = jest.fn();
+
+    beforeEach(() => {
+        useRouter.mockReturnValue({
+            push: mockPush,
+        });
+    });
+    afterEach(() => {
+        jest.clearAllMocks(); // Limpia los mocks después de cada prueba
+    });
+
+    it('should render inputs and button correctly', () => {
+        const { getByText, getByPlaceholderText } = render(<Home />);
+        expect(getByText('Para los Alumnos')).toBeTruthy();
+        expect(getByText('En esta plataforma, podrás consultar las calificaciones de las prácticas que te asignan tus docentes. Revisa tus avances y mantente al tanto de tus resultados en tiempo real.')).toBeTruthy();
+        expect(getByText('Para los Docentes')).toBeTruthy();
+        expect(getByText('Como docente, tendrás la posibilidad de crear actividades y prácticas personalizadas para tus alumnos, y evaluarlas en tiempo real.')).toBeTruthy();
+        
+    });
+
+   
+});
---
+++ __tests__/perfil.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import Perfil from "../app/(tabs)/perfil";
+import { useRouter } from 'expo-router';
+import { Alert } from 'react-native'; // Importa Alert de react-native
+
+// Mocks de dependencias
+jest.mock('expo-router', () => ({
+    useRouter: jest.fn(),
+    useNavigation: jest.fn(() => ({
+      dispatch: jest.fn(),
+    })),
+    Stack: {
+      Screen: ({ children }) => <>{children}</>, // Mock de Stack.Screen
+    },
+}));
+
+// Definir mock para `useAuth` con `userData` y `logout`
+jest.mock('../app/auth/useAuth', () => ({
+  useAuth: jest.fn(() => ({
+    userData: {
+      vchNombre: 'John',
+      vchAPaterno: 'Doe',
+      vchAMaterno: 'Smith',
+      vchEmail: 'johndoe@example.com',
+      vchFotoPerfil: 'userProfile.png',
+      vchNombreRol: 'Docente',
+      vchMatricula: '20210643',
+    },
+    logout: jest.fn(),
+  })),
+}));
+
+// Mock de `Alert` para evitar interrupciones durante las pruebas
+jest.spyOn(Alert, 'alert').mockImplementation((title, message, buttons) => {
+  const logoutButton = buttons.find(button => button.text === "Cerrar sesión");
+  if (logoutButton && logoutButton.onPress) {
+    logoutButton.onPress();
+  }
+});
+
+describe("Perfil renderizado", () => {
+    it('should render user information correctly', () => {
+        const { getByText } = render(<Perfil />);
+        
+        // Verifica que los textos se rendericen correctamente con los datos de `userData`
+        expect(getByText('John Doe Smith')).toBeTruthy();
+        expect(getByText('Correo electrónico:')).toBeTruthy();
+        expect(getByText('johndoe@example.com')).toBeTruthy();
+        expect(getByText('Estado de cuenta:')).toBeTruthy();
+        //expect(getByText('Activo')).toBeTruthy();
+    });
+
+   
+});
