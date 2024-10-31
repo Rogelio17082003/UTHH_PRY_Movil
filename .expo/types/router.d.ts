a25c14ad871658c456ba759ec3d0eebfb90970b0
Rogelio <20210663@uthh.edu.mx>
2024-10-30 22:21:35 -0600

    recuperado

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/perfil` | `/Login` | `/_sitemap` | `/auth/AuthContext` | `/auth/sessionService` | `/auth/tokenUtils` | `/auth/useAuth` | `/home` | `/nav` | `/pages/Home` | `/pages/Login` | `/pages/nav` | `/perfil`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/perfil` | `/Login` | `/_sitemap` | `/auth/AuthContext` | `/auth/sessionService` | `/auth/tokenUtils` | `/auth/useAuth` | `/home` | `/nav` | `/perfil`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { View, Text } from "react-native";
 export default function index() {
     return (
         <View>
-      //
+      
             <Home />
         </View>
 
